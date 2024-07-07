f6c9e598fb53fa4ed035a3e2030fe738d8ce3af9
Brandon Woodford <brandon.woodford.work@gmail.com>
2024-07-07 15:19:40 -0500

    Add initial example of single song selection working
    This commit is able to display artists, albums, and songs. And allow the
    user to select a single song for playback.

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from multipart.multipart import os
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
+root_path = "/media/music"
 
-def get_song_generator(start, end):
-    with open("/media/music/Bas/Too High to Riot/09 Night Job.flac", mode="rb") as song:
+def get_song_generator(path, start, end):
+    with open(path, mode="rb") as song:
         song.seek(start)
         yield song.read(end-start)
 
@@ -19,24 +19,61 @@ def get_song_generator(start, end):
 def home_page(request: Request):
     return templates.TemplateResponse('home/index.html', {"request": request})
 
-@app.get("/audio/selection")
-def search_books():
+@app.get("/artists")
+def get_artists():
+
+    # TODO: Setup path validation
+    artists = os.listdir(path=root_path)
+    artists.sort()
+
+    content = ""
+    for artist in artists:
+        content += f"<button hx-get='/artists/{artist}/albums' hx-target='#music-display' hx-trigger='click'>{artist}</button>"
+
+    return HTMLResponse(content)
+
+@app.get("/artists/{artist}/albums")
+def get_albums(artist: str):
+
+    # TODO: Setup path validation
+    albums = os.listdir(path=root_path + "/" + artist)
+    albums.sort()
+
+    content = ""
+    for album in albums:
+        content += f"<button hx-get='/artists/{artist}/albums/{album}' hx-target='#music-display'>{album}</button>"
+
+    return HTMLResponse(content)
+
+@app.get("/artists/{artist}/albums/{album}")
+def get_songs(artist: str, album: str):
+
+    # TODO: Setup path validation
+    songs = os.listdir(path=root_path + "/" + artist + "/" + album)
+    songs.sort()
+
+    content = "<div id='songs'>"
+    for song in songs:
+        content += f"<button hx-get='/artists/{artist}/albums/{album}/songs/{song}' hx-target='#music-display'>{song}</button>"
+
+    content += "</div>"
+
+    return HTMLResponse(content)
+
+@app.get("/artists/{artist}/albums/{album}/songs/{song}")
+def get_audio_element(artist: str, album: str, song: str):
     return HTMLResponse(
-        """
-        <div>
-            <audio controls preload="auto" src="/audio"></audio>
-        </div>
-        """
+        f"<audio autoplay controls preload='auto' src='/artists/{artist}/albums/{album}/songs/{song}/audio'></audio>"
     )
 
-@app.get("/audio")
-async def get_audio(request: Request):
+@app.get("/artists/{artist}/albums/{album}/songs/{song}/audio")
+def get_audio_stream(request: Request, artist: str, album: str, song: str):
     range_header = request.headers.get("range")
-    print(f"range-header: {range_header}")
-
     headers = {"Accept-Ranges": "bytes"}
 
-    size = os.path.getsize("/media/music/Bas/Too High to Riot/09 Night Job.flac")
+    # TODO: Setup path validation
+    path = f"{root_path}/{artist}/{album}/{song}"
+    size = os.path.getsize(path)
 
     start = 0
     end = size
@@ -46,4 +83,4 @@ async def get_audio(request: Request):
         headers["Content-Length"] = f"{end-start}"
         headers["Content-Range"] = f"bytes {start}-{end}/{size}"
 
-    return StreamingResponse(get_song_generator(start, end), media_type="audio/flac", headers=headers)
+    return StreamingResponse(get_song_generator(path, start, end), media_type="audio/flac", headers=headers)
